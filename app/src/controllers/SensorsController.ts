f93d5b877aa653dddab592b75f7653f93871da19
Gwendal Gourmelon <ggourmelon@prodival.com>
2019-10-29 11:22:02 +0100

    get to post

--- app/src/controllers/SensorsController.ts
+++ app/src/controllers/SensorsController.ts
@@ -8,14 +8,13 @@ import GenericController from "./GenericController"
 export default class SensorsController extends GenericController {
     constructor(path:string, Model:any){
         super(path, Model)
-        this.router.get(`${this.path}/:id/statements`, (...args) => this.pushValues(...args))
+        this.router.post(`${this.path}/:id/statements`, (...args) => this.pushValues(...args))
     }
     public pushValues(req:Request, res:Response,next:any){
         this.Model.findOne({ id: req.params.id }, (err: any, sensors: any) => {
             sensors.statements.push([new Date(), ...req.body]);
             this.Model.updateOne({id: req.params.id}, sensors, (err:any,affected:any,resp:any) => {
                 res.status(200).json({affected:affected})
-                console.log(err,affected,resp)
             })
         })
     }
